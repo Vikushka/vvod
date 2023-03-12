7935a33edeac6ba8081772967282b2cbf716d1f0
R2 <R2@MILA-SHKA>
2023-03-12 21:33:45 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя");
+            string name= Console.ReadLine();
+            Console.WriteLine("ваше имя " + name);
+          
+
+        }
+    }
+}
